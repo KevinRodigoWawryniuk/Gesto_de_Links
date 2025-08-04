0b9d3637a3e6dc9356d3dad1911a456b04fb4f55
Kevin Rodrigo <126299266+KevinRodigoWawryniuk@users.noreply.github.com>
2025-08-04 15:24:42 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+function ClickLink(params) {
+    switch (params) {
+        case "Fiverr":
+            window.open('https://es.fiverr.com/s/389xlN9');
+            break;
+        case "Github":
+            window.open('https://github.com/KevinRodigoWawryniuk');
+            break;
+        case "Instagram":
+            window.open('https://www.instagram.com/kevin_wawryniuk/');
+            break;
+        case "Linkedin":
+            window.open('https://www.linkedin.com/in/kevin-rodrigo-wawryniuk-70447a352/');
+            break;
+        case "Portafolio":
+            window.open('https://kevinrodigowawryniuk.github.io/portafoliopages/');
+            break;
+        case "Twitch":
+            window.open('https://www.twitch.tv/devplayerone');
+            break;
+    }
+};
